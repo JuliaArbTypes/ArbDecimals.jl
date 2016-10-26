67c5ab2c6307f1761b66304184048553cb2c7b59
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-10-26 07:07:33 -0400

    Update show.jl

--- src/io/show.jl
+++ src/io/show.jl
@@ -16,24 +16,24 @@ show{D,P}(x::ArbDec{D,P}, digs::Int) = show(STDOUT, x, digs)
 
 
 function showall{D,P}(io::IO, x::ArbDec{D,P})
-    str = stringall(x)
+    str = string_all(x)
     print(io, str)
 end
 showall{D,P}(x::ArbDec{D,P}) = showall(STDOUT, x)
 
 function showcompact{D,P}(io::IO, x::ArbDec{D,P})
-    str = stringcompact(x)
+    str = string_compact(x)
     print(io, str)
 end
 showcompact{D,P}(x::ArbDec{D,P}) = showcompact(STDOUT, x)
 
-function show_small{D,P}(io::IO, x::ArbDec{D,P})
+function showsmall{D,P}(io::IO, x::ArbDec{D,P})
     str = string_small(x)
     print(io, str)
 end
 show_small{D,P}(x::ArbDec{D,P}) = show_small(STDOUT, x)
 
-function show_large{D,P}(io::IO, x::ArbDec{D,P})
+function showlarge{D,P}(io::IO, x::ArbDec{D,P})
     str = string_large(x)
     print(io, str)
 end
