12615e8d91509de2e867bd61fc23b845b7f2b635
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-10-26 06:56:29 -0400

    Update string.jl

--- src/io/string.jl
+++ src/io/string.jl
@@ -4,6 +4,13 @@ function stringall{D,P}(x::ArbDec{D,P})
     return str
 end
 
+function stringcompact{D,P}(x::ArbDec{D,P})
+    digs = min(D, @digitsForCompactStrings())
+    cstr = ccall(@libarb(arb_get_str), Ptr{UInt8}, (Ptr{ArbDec}, Int, UInt), &x, D, 2%UInt)
+    str = unsafe_string(cstr)
+    return str
+end
+
 function string{D,P}(x::ArbDec{D,P})
     cstr = ccall(@libarb(arb_get_str), Ptr{UInt8}, (Ptr{ArbDec}, Int, UInt), &x, D, 2%UInt)
     str = unsafe_string(cstr)
