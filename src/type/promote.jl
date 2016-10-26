62983eb04d8c8bfae89e50ec74ea7a8af9a5a0bf
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-10-26 02:29:02 -0400

    Create promote.jl

---
+++ src/type/promote.jl
@@ -0,0 +1,15 @@
+# Promotion
+for T in (:Int128, :Int64, :Int32, :Int16, :Float64, :Float32, :Float16,
+          :(Rational{Int64}), :(Rational{Int32}), :(Rational{Int16}),
+          :String)
+  @eval promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{$T}) = ArbDec{D,P}
+end
+
+float{D,P}(x::ArbDec{D,P}) = x
+
+promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{BigFloat}) = ArbDec{D,P}
+promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{BigInt}) = ArbDec{D,P}
+promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{Rational{BigInt}}) = Rational{BigInt}
+
+promote_rule{D1,P1,D2,P2}(::Type{ArbDec{D1,P1}}, ::Type{ArbDec{D2,P2}}) =
+    ifelse(D1>D2, ArbDec{D1,P1}, ArbDec{D2,P2})
