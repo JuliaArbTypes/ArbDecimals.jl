ea3ce16196d584100d781c86299d32f6dbf9ed38
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-10-26 06:51:11 -0400

    Update show.jl

--- src/io/show.jl
+++ src/io/show.jl
@@ -1,3 +1,9 @@
+function showall{D,P}(io::IO, x::ArbDec{D,P})
+    str = stringall(x)
+    print(io, str)
+end
+showall{D,P}(x::ArbDec{D,P}) = showall(STDOUT, x)
+
 function show{D,P}(io::IO, x::ArbDec{D,P})
     str = string(x)
     print(io, str)
