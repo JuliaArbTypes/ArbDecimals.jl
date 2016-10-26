7e85e588de69a61206e59fd7cc82a751c67cd1a4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-10-26 07:05:32 -0400

    Update show.jl

--- src/io/show.jl
+++ src/io/show.jl
@@ -1,3 +1,20 @@
+#=
+function show{D,P}(io::IO, x::ArbDec{D,P})
+    str = string(x)
+    print(io, str)
+end
+show{D,P}(x::ArbDec{D,P}) = show(STDOUT, x)
+=#
+show{D,P}(io::IO, x::ArbDec{D,P}) = show_small(io, x)
+show{D,P}(x::ArbDec{D,P}) = show_small(STDOUT, x)
+
+function show{D,P}(io::IO, x::ArbDec{D,P}, digs::Int)
+    str = string(x, digs)
+    print(io, str)
+end
+show{D,P}(x::ArbDec{D,P}, digs::Int) = show(STDOUT, x, digs)
+
+
 function showall{D,P}(io::IO, x::ArbDec{D,P})
     str = stringall(x)
     print(io, str)
@@ -22,15 +39,3 @@ function show_large{D,P}(io::IO, x::ArbDec{D,P})
 end
 show_large{D,P}(x::ArbDec{D,P}) = show_large(STDOUT, x)
 
-
-function show{D,P}(io::IO, x::ArbDec{D,P})
-    str = string(x)
-    print(io, str)
-end
-show{D,P}(x::ArbDec{D,P}) = show(STDOUT, x)
-
-function show{D,P}(io::IO, x::ArbDec{D,P}, digs::Int)
-    str = string(x, digs)
-    print(io, str)
-end
-show{D,P}(x::ArbDec{D,P}, digs::Int) = show(STDOUT, x, digs)
