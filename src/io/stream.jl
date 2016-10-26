9201cd8308f29db693346030e149cdc5c4529e0a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2016-10-26 02:34:31 -0400

    Update stream.jl

--- src/io/stream.jl
+++ src/io/stream.jl
@@ -1,1 +1,8 @@
+function serialize{D,P}(ser::AbstractSerializer, a::ArbDec{D,P})
+    serialize_type(ser, T)
+    write(ser.io, string_typed(a))
+end
 
+function deserialize{D,P}(ser::AbstractSerializer, ::Type{ArbDec{D,P}})
+    parse( read(ser.io, String) )
+end
