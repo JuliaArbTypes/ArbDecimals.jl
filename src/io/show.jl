e947cd9c227854a119525fa300516e6da41da564
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-01-10 11:17:19 -0500

    rename

--- src/io/show.jl
+++ src/io/show.jl
@@ -5,8 +5,8 @@ function show{D,P}(io::IO, x::ArbDec{D,P})
 end
 show{D,P}(x::ArbDec{D,P}) = show(STDOUT, x)
 =#
-show{D,P}(io::IO, x::ArbDec{D,P}) = showsmall(io, x)
-show{D,P}(x::ArbDec{D,P}) = showsmall(STDOUT, x)
+show{D,P}(io::IO, x::ArbDec{D,P}) = show_small(io, x)
+show{D,P}(x::ArbDec{D,P}) = show_small(STDOUT, x)
 
 function show{D,P}(io::IO, x::ArbDec{D,P}, digs::Int)
     str = string(x, digs)
@@ -15,27 +15,27 @@ end
 show{D,P}(x::ArbDec{D,P}, digs::Int) = show(STDOUT, x, digs)
 
 
-function showall{D,P}(io::IO, x::ArbDec{D,P})
-    str = stringall(x)
+function show_all{D,P}(io::IO, x::ArbDec{D,P})
+    str = string_all(x)
     print(io, str)
 end
-showall{D,P}(x::ArbDec{D,P}) = showall(STDOUT, x)
+show_all{D,P}(x::ArbDec{D,P}) = show_all(STDOUT, x)
 
-function showcompact{D,P}(io::IO, x::ArbDec{D,P})
+function show_compact{D,P}(io::IO, x::ArbDec{D,P})
     str = string_compact(x)
     print(io, str)
 end
-showcompact{D,P}(x::ArbDec{D,P}) = showcompact(STDOUT, x)
+show_compact{D,P}(x::ArbDec{D,P}) = show_compact(STDOUT, x)
 
-function showsmall{D,P}(io::IO, x::ArbDec{D,P})
-    str = stringsmall(x)
+function show_small{D,P}(io::IO, x::ArbDec{D,P})
+    str = string_small(x)
     print(io, str)
 end
-showsmall{D,P}(x::ArbDec{D,P}) = showsmall(STDOUT, x)
+show_small{D,P}(x::ArbDec{D,P}) = show_small(STDOUT, x)
 
-function showlarge{D,P}(io::IO, x::ArbDec{D,P})
-    str = stringlarge(x)
+function show_large{D,P}(io::IO, x::ArbDec{D,P})
+    str = string_large(x)
     print(io, str)
 end
-showlarge{D,P}(x::ArbDec{D,P}) = showlarge(STDOUT, x)
+show_large{D,P}(x::ArbDec{D,P}) = show_large(STDOUT, x)
 
