c4e979bea1975c3e621dea2ee92d99e2c0a14159
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-01-10 06:42:40 -0500

    fix subtle promote_rule bug

--- src/type/promote.jl
+++ src/type/promote.jl
@@ -2,13 +2,13 @@
 for T in (:Int128, :Int64, :Int32, :Int16, :Float64, :Float32, :Float16,
           :(Rational{Int64}), :(Rational{Int32}), :(Rational{Int16}),
           :String)
-  @eval promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{$T}) = ArbDec{D,P}
+  @eval promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{$T}) = ArbDec
 end
 
 float{D,P}(x::ArbDec{D,P}) = x
 
-promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{BigFloat}) = ArbDec{D,P}
-promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{BigInt}) = ArbDec{D,P}
+promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{BigFloat}) = ArbDec
+promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{BigInt}) = ArbDec
 promote_rule{D,P}(::Type{ArbDec{D,P}}, ::Type{Rational{BigInt}}) = Rational{BigInt}
 
 promote_rule{D1,P1,D2,P2}(::Type{ArbDec{D1,P1}}, ::Type{ArbDec{D2,P2}}) =
